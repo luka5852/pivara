ac39a5367d68307a18f0f7edbc1026ca6129801f
Radunovic Luka <luka.radunovic@vegait.rs>
2022-11-03 16:28:20 +0100

    Prije konektovanja sa senzorima

--- pivara/data/main.js
+++ pivara/data/main.js
@@ -1,13 +1,12 @@
-let json_vrijeme_temp;
-
 let temperatura_trazena;
 let vrijeme_trazeno;
-
 
 const state = {
     pumpa_piva: JSON.parse(localStorage.getItem('pumpa_piva')) || false,
     pumpa_vode: JSON.parse(localStorage.getItem('pumpa_vode')) || false, 
-    stanje_pivare: JSON.parse(localStorage.getItem('stanje_pivare')) || false
+    stanje_pivare: JSON.parse(localStorage.getItem('stanje_pivare')) || false,
+    trazeno_vrijeme: JSON.parse(localStorage.getItem('trazeno_vrijeme')) || 0,
+    trazena_temperatura: localStorage.getItem('trazena_temperatura') || 0
 }
 
 const setState = (key, value) => {
@@ -26,9 +25,8 @@ loadApp();
 function upisati_temp_vrijeme() {
     temperatura_trazena = document.getElementById("temperatura_trazena").value;
     vrijeme_trazeno = document.getElementById("vrijeme_trazeno").value;
-    if (vrijeme_trazeno == '' || vrijeme_trazeno == null) {
+    if (vrijeme_trazeno == '' || vrijeme_trazeno == null || vrijeme_trazeno < 0) {
         vrijeme_trazeno = 0;
-        localStorage.setItem('vrijeme_trazeno', 0)
     }
     if (temperatura_trazena < 0) {
         temperatura_trazena = 0;
@@ -36,20 +34,24 @@ function upisati_temp_vrijeme() {
     else if (temperatura_trazena > 100) {
         temperatura_trazena = 99;
     }
-    json_vrijeme_temp = JSON.stringify({
-        temperatura_trazena: temperatura_trazena,
-        vrijeme_trazeno: vrijeme_trazeno
-    });
-
-    localStorage.setItem('temperatura_trazena', temperatura_trazena)
-    localStorage.setItem('vrijeme_trazeno', vrijeme_trazeno)
-
-
-    window.alert(json_vrijeme_temp);
+    if(temperatura_trazena == ''){
+        alert('Greska pri unosu temperature');
+    }
+    else{
+        setState('trazeno_vrijeme', vrijeme_trazeno);
+        setState('trazena_temperatura', temperatura_trazena);        
+    }
+    
+}
+function upisati_vrijednost_u_labelu(tmp_id, vrijednost){
+    document.getElementById(tmp_id).innerHTML = vrijednost;
 }
 function start() {
     background_boja_zelena("indikator_rada");
     setState('stanje_pivare', true);
+    upisati_vrijednost_u_labelu('labela_trazena_temperatura', state.trazena_temperatura);
+    upisati_vrijednost_u_labelu('labela_trazeno_vrijeme', state.trazena_temperatura);
+
 }
 function stop() {
     setState('stanje_pivare', false);
@@ -59,12 +61,12 @@ function stop() {
 function prekidac_pumpa_piva() {
     setState('pumpa_piva',!state.pumpa_piva)
     upis_boje(state.pumpa_piva,"labela_pumpa_piva");
-    location.href = '/PUMPA_PIVA';
+    //location.href = '/PUMPA_PIVA';
 }
 function prekidac_pumpa_vode() {
     setState('pumpa_vode',!state.pumpa_vode)
     upis_boje(state.pumpa_vode, "labela_pumpa_vode");
-    location.href = '/PUMPA_VODE';
+    //location.href = '/PUMPA_VODE';
 }
 
 function upis_boje(state, id){
